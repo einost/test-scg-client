4346a784b8a2a0c860e3e4d4eaf1a9c0ba917859
Fakeitem <non_scoobydoo@hotmail.com>
2021-02-15 02:11:01 +0700

    fix: scss is not working

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,12 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~/assets/scss/main.scss'],
+
+  // @nuxtjs/style-resources
+  styleResources: {
+    scss: ['./assets/scss/*.scss']
+  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -44,7 +49,9 @@ export default {
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
-    '@nuxt/content'
+    '@nuxt/content',
+    // 3rd party
+    '@nuxtjs/style-resources'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
